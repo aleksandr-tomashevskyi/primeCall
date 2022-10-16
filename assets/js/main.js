5942b5ae7d1194fde5adad186cd061cdb4c0ba96
aleksandr-tomashevskyi <oleksandr.o.tomashevskyi@gmail.com>
2022-10-16 11:33:18 +0300

    refactored code to use OOP for hero

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,49 +6,66 @@ let heroFrame = 0;
 let heroFrameModifier = 1;
 let currentHeroAction = 'idle';
 
-function heroIdleAnim(){
-   heroImage.style.objectPosition = `-${heroFrame*120.3+33}px -1px`;
-   heroFrame += heroFrameModifier;
-   if(heroFrame == 11){
-      heroFrameModifier = heroFrameModifier * -1;
-   } else if(heroFrame < 1){
-      heroFrameModifier = heroFrameModifier * -1;
+class Hero{
+   constructor(image, container, frame, frameModifier, currentAction, footPrintContainer, footPrintPosX, footPrintPosY, footPrintWidth, footPrintHeight){
+      this.image = image;
+      this.container = container;
+      this.frame = frame;
+      this.frameModifier = frameModifier;
+      this.currentAction = currentAction;
+      this.footPrintContainer = footPrintContainer;
+      this.footPrintPosX = footPrintPosX;
+      this.footPrintPosY = footPrintPosY;
+      this.footPrintWidth = footPrintWidth;
+      this.footPrintHeight = footPrintHeight;
    }
-   setTimeout(()=>window.requestAnimationFrame(animFrameHandler), 150);
+
+   idleAnim(){
+      this.image.style.objectPosition = `-${this.frame*120.3+33}px -1px`;
+      this.frame += this.frameModifier;
+      if(this.frame == 11){
+         this.frameModifier = this.frameModifier * -1;
+      } else if(this.frame < 1){
+         this.frameModifier = this.frameModifier * -1;
+      }
+      setTimeout(()=>window.requestAnimationFrame(animFrameHandler), 150);
+   }
+
+   moveAnim(){
+      if(this.currentAction === 'MR'){
+         this.image.style.objectPosition = `-${this.frame*120.3+33}px -83px`;
+         this.frame += this.frameModifier;
+         if(this.frame === 18){
+            this.frame = 0;
+         }
+      }
+      setTimeout(()=>window.requestAnimationFrame(animFrameHandler), 40);
+   }
 }
+
+let heroInstance = new Hero(document.querySelector('.hero__image'), document.querySelector('.hero__container'), 0, 1, 'idle', null, null, null, null);
+
 
 function animFrameHandler(){
-   if(currentHeroAction === 'idle'){
-      heroIdleAnim();
+   if(heroInstance.currentAction === 'idle'){
+      heroInstance.idleAnim();
    } else{
-      heroMovementAnim();
+      heroInstance.moveAnim();
    }
 }
 
-function heroMovementAnim(){
-   if(currentHeroAction === 'MR'){
-      heroImage.style.objectPosition = `-${heroFrame*120.3+33}px -83px`;
-      heroFrame += heroFrameModifier;
-      console.log('Hero Frame: ' + heroFrame);
-      console.log('Hero frame modifier: '+heroFrameModifier);
-      if(heroFrame === 18){
-         heroFrame = 0;
-      }
-   }
-   setTimeout(()=>window.requestAnimationFrame(animFrameHandler), 40);
-}
 
 document.addEventListener('keydown', (event) => {
    if(event.key === 'ArrowRight'){
-      currentHeroAction = 'MR'; //MR stands for Movement Right
+      heroInstance.currentAction = 'MR'; //MR stands for Movement Right
    } else if(event.key === 'ArrowLeft'){
-      currentHeroAction = 'ML'; //ML stands for Movement Left
+      heroInstance.currentAction = 'ML'; //ML stands for Movement Left
    }
 });
 document.addEventListener('keyup', (event) => {
-   currentHeroAction = 'idle';
-   heroFrame = 0;
-   heroFrameModifier = 1;
+   heroInstance.currentAction = 'idle';
+   heroInstance.frame = 0;
+   heroInstance.frameModifier = 1;
 });
 
 window.requestAnimationFrame(animFrameHandler);
